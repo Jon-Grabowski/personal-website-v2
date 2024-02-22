c134eddb12775008ec92624c890ad808a939d306
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-02-22 13:05:56 -0500

    init commit

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './content.css'
+
+function Content() {
+    return (
+        <div id='content-wrapper'>
+            <div>
+                <p>Content Goes Here</p>
+            </div>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <div id='nav-bar-wrapper' className=''>
+            <div>
+                <p>image goes here</p>
+            </div>
+            <div>
+                <p>links go here</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
