867f992ae745263be791ec9bcae8a82da9e0eafb
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-06-20 22:00:46 -0400

    created NameHeadline component

---
+++ src/components/Home/Name/NameHeadline.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NameHeadline() {
+    return (
+        <div className='text-end'>
+            {/* <p>"I find joy in blending creativity with the art of problem-solving through code."</p> */}
+            <p>"Tagline"</p>
+
+        </div>
+    )
+}
+
+export default NameHeadline
